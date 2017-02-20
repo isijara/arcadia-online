f53f8866ccd28f3787b3a3e6c10bb099087bbf37
Ramón Alberto Isijara Medina <raisijaram@concredito.com.mx>
2017-02-20 01:59:01 -0700

    Correction on GameCharacterFactory

--- src/main/java/characters/GameCharacterFactory.java
+++ src/main/java/characters/GameCharacterFactory.java
@@ -4,22 +4,22 @@ public class GameCharacterFactory {
 
     public GameCharacter create(String type) throws Exception{
 
-        if( type.equals(GameCharacterClass.KNIGHT))
+        if( type.equals(GameCharacterClass.KNIGHT.toString()))
             return new Knight();
-        else if (type.equals(GameCharacterClass.COMMANDER))
+        else if (type.equals(GameCharacterClass.COMMANDER.toString()))
             return new Commander();
-        else if (type.equals(GameCharacterClass.TROLL))
+        else if (type.equals(GameCharacterClass.TROLL.toString()))
             return new Troll();
-        if( type.equals(GameCharacterClass.ARCHER))
+        if( type.equals(GameCharacterClass.ARCHER.toString()))
             return new Archer();
-        else if (type.equals(GameCharacterClass.WIZARD))
+        else if (type.equals(GameCharacterClass.WIZARD.toString()))
             return new Commander();
-        else if (type.equals(GameCharacterClass.OGRE))
+        else if (type.equals(GameCharacterClass.OGRE.toString()))
             return new Ogre();
-        else if (type.equals(GameCharacterClass.PAWN))
+        else if (type.equals(GameCharacterClass.PAWN.toString()))
             return new Pawn();
         else
-            throw new Exception("GameCharacterClass " + type + "does not exist");
+            throw new Exception("GameCharacterClass " + type + " does not exist");
     }
 
 }
